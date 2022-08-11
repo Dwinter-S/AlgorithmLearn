27fb2c3414d0f229d515dc3c086d40f266dd7ab4
Mac mini <suddcn@gmail.com>
2022-08-11 18:26:47 +0800

    update

--- AlgorithmLearn/HashTable.swift
+++ AlgorithmLearn/HashTable.swift
@@ -41,4 +41,35 @@ class HashTable {
         return false
     }
     
+    // MARK: - 633. 平方数之和（中等）
+    /*
+     给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。
+     
+     示例 1：
+     输入：c = 5
+     输出：true
+     解释：1 * 1 + 2 * 2 = 5
+     
+     示例 2：
+     输入：c = 3
+     输出：false
+     
+     提示：
+     0 <= c <= 231 - 1
+     */
+    static func judgeSquareSum(_ c: Int) -> Bool {
+        var left = 0
+        var right = Int(sqrt(Double(c)))
+        while left <= right {
+            let sum = left * left + right * right
+            if sum == c {
+                return true
+            } else if sum > c {
+                right -= 1
+            } else {
+                left += 1
+            }
+        }
+        return false
+    }
 }
