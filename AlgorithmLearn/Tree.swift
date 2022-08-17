f0051cbd564044a3bef1b02c36a86c764bb39288
sudd <suddcn@163.com>
2022-08-18 02:15:17 +0800

    update

--- AlgorithmLearn/Tree.swift
+++ AlgorithmLearn/Tree.swift
@@ -20,6 +20,15 @@ public class TreeNode {
     }
 }
 
+public class Node {
+    public var val: Int
+    public var children: [Node]
+    public init(_ val: Int) {
+        self.val = val
+        self.children = []
+    }
+}
+
 class Tree {
     // MARK: - 100. 相同的树
     static func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
@@ -276,7 +285,7 @@ class Tree {
         return maxDiameter
     }
     
-    // MARK: - 654. 最大二叉树
+    // MARK: - 654. 最大二叉树（中等）
     static func constructMaximumBinaryTree(_ nums: [Int]) -> TreeNode? {
         func constructMaximumBinaryTree(_ nums: [Int], left: Int, right: Int) -> TreeNode? {
             if left > right { return nil }
@@ -291,8 +300,134 @@ class Tree {
         return constructMaximumBinaryTree(nums, left: 0, right: nums.count - 1)
     }
     
-    // MARK: - 687. 最长同值路径
+    // MARK: - 687. 最长同值路径（中等）
     static func longestUnivaluePath(_ root: TreeNode?) -> Int {
-        return 0
+        var ans = 0
+        
+        func dfs(_ root: TreeNode?) -> Int {
+            guard let root = root else { return 0 }
+            var leftPath = dfs(root.left)
+            var rightPath = dfs(root.right)
+            if root.val == root.left?.val {
+                leftPath += 1
+            } else {
+                leftPath = 0
+            }
+            if root.val == root.right?.val {
+                rightPath += 1
+            } else {
+                rightPath = 0
+            }
+            let curPath = max(leftPath, rightPath)
+            if root.val == root.left?.val && root.val == root.right?.val {
+                ans = max(ans, leftPath + rightPath)
+            } else {
+                ans = max(ans, curPath)
+            }
+            return curPath
+        }
+        dfs(root)
+        return ans
+    }
+    
+    // MARK: - 87. 扰乱字符串（困难）
+    
+    // MARK: - 102. 二叉树的层序遍历（中等）
+    static func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [] }
+        var ans = [[Int]]()
+        var queue: [TreeNode] = [root]
+        while !queue.isEmpty {
+            var arr = [Int]()
+            var newNodes = [TreeNode]()
+            for node in queue {
+                arr.append(node.val)
+                if let left = node.left {
+                    newNodes.append(left)
+                }
+                if let right = node.right {
+                    newNodes.append(right)
+                }
+            }
+            ans.append(arr)
+            queue = newNodes
+        }
+        return ans
+    }
+    
+    // MARK: - 429. N 叉树的层序遍历（中等）
+    static func levelOrder(_ root: Node?) -> [[Int]] {
+        guard let root = root else { return [] }
+        var ans = [[Int]]()
+        var queue: [Node] = [root]
+        while !queue.isEmpty {
+            var arr = [Int]()
+            var newNodes = [Node]()
+            for node in queue {
+                arr.append(node.val)
+                for child in node.children {
+                    newNodes.append(child)
+                }
+            }
+            ans.append(arr)
+            queue = newNodes
+        }
+        return ans
+    }
+    
+    // MARK: - 690. 员工的重要性（中等）
+    /*
+     给定一个保存员工信息的数据结构，它包含了员工 唯一的 id ，重要度 和 直系下属的 id 。
+     比如，员工 1 是员工 2 的领导，员工 2 是员工 3 的领导。他们相应的重要度为 15 , 10 , 5 。那么员工 1 的数据结构是 [1, 15, [2]] ，员工 2的 数据结构是 [2, 10, [3]] ，员工 3 的数据结构是 [3, 5, []] 。注意虽然员工 3 也是员工 1 的一个下属，但是由于 并不是直系 下属，因此没有体现在员工 1 的数据结构中。
+     现在输入一个公司的所有员工信息，以及单个员工 id ，返回这个员工和他所有下属的重要度之和。
+     
+     示例：
+     输入：[[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+     输出：11
+     解释：
+     员工 1 自身的重要度是 5 ，他有两个直系下属 2 和 3 ，而且 2 和 3 的重要度均为 3 。因此员工 1 的总重要度是 5 + 3 + 3 = 11 。
+     
+     提示：
+     一个员工最多有一个 直系 领导，但是可以有多个 直系 下属
+     员工数量不超过 2000 。
+     */
+    public class Employee {
+        public var id: Int
+        public var importance: Int
+        public var subordinates: [Int]
+        public init(_ id: Int, _ importance: Int, _ subordinates: [Int]) {
+            self.id = id
+            self.importance = importance
+            self.subordinates = subordinates
+        }
+    }
+    static func getImportance(_ employees: [Employee], _ id: Int) -> Int {
+        guard let curEmployee = employees.first(where: { $0.id == id }) else { return 0 }
+        var ans = 0
+        var subordinates = curEmployee.subordinates
+        while !subordinates.isEmpty {
+            let subId = subordinates.removeFirst()
+            let e = employees.first(where: { $0.id == subId })!
+            ans += e.importance
+            if !e.subordinates.isEmpty {
+                subordinates.append(contentsOf: e.subordinates)
+            }
+        }
+        return curEmployee.importance + ans
+    }
+    
+    // MARK: - N 叉树的最大深度
+    static func maxDepth(_ root: Node?) -> Int {
+        guard let root = root else { return 0 }
+        var ans = 1
+        for child in root.children {
+            ans = max(ans, maxDepth(child) + 1)
+        }
+        return ans
+    }
+    
+    // MARK: - 662. 二叉树最大宽度（中等）
+    static func widthOfBinaryTree(_ root: TreeNode?) -> Int {
+       return 0
     }
 }
