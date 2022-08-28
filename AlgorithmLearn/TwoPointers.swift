22a2177a2ec2c93be70b81e7b89672a297eae8dd
sudd <suddcn@163.com>
2022-08-28 21:55:27 +0800

    update

--- AlgorithmLearn/TwoPointers.swift
+++ AlgorithmLearn/TwoPointers.swift
@@ -423,4 +423,435 @@ class TwoPointers {
         }
         return ans
     }
+    
+    // MARK: - 187. 重复的DNA序列
+    static func findRepeatedDnaSequences(_ s: String) -> [String] {
+        // 哈希表
+        /*
+        guard s.count > 10 else { return [] }
+        var ans = [String]()
+        let chars = Array(s)
+        let len = chars.count
+        var stringCountMap = [String : Int]()
+        for i in 0..<len-10+1 {
+            let subStr = String(chars[i..<i+10])
+            stringCountMap[subStr] = (stringCountMap[subStr] ?? 0) + 1
+            if stringCountMap[subStr] == 2 {
+                ans.append(subStr)
+            }
+        }
+        return ans
+         */
+        
+        // 滑动窗口
+        guard s.count > 10 else { return [] }
+        var ans = [String]()
+        let chars = Array(s)
+        let len = chars.count
+        // 四个字符用2比特的数字表示
+        let cIntMap = ["A" : 0, "C" : 1, "G" : 2, "T" : 3]
+        var x = 0
+        // 前九个字符
+        for i in 0..<9 {
+            x = (x << 2) | cIntMap[String(chars[i])]!
+        }
+        var numCountMap = [Int : Int]()
+        for i in 0..<len-10+1 {
+            // 滑动窗口：& ((1 << 20) - 1)将20位之前的数字置位0
+            x = ((x << 2) | cIntMap[String(chars[i + 9])]!) & ((1 << 20) - 1)
+            numCountMap[x] = (numCountMap[x] ?? 0) + 1
+            if numCountMap[x] == 2 {
+                ans.append(String(chars[i..<i+10]))
+            }
+        }
+        return ans
+    }
+    
+    // MARK: - 643. 子数组最大平均数 I
+    static func findMaxAverage(_ nums: [Int], _ k: Int) -> Double {
+        // 求所有子数组的和
+        /*
+        var maxSum = 0
+        for i in 0..<nums.count-k+1 {
+            var curSum = 0
+            for j in i..<i+k {
+                curSum += nums[j]
+            }
+            maxSum = max(maxSum, curSum)
+        }
+        return Double(maxSum) / Double(k)
+         */
+        
+        // 滑动窗口
+        var curSum = 0
+        for i in 0..<k {
+            curSum += nums[i]
+        }
+        var maxSum = curSum
+        for i in 0..<nums.count-k {
+            curSum = curSum + nums[i+k] - nums[i]
+            maxSum = max(maxSum, curSum)
+        }
+        return Double(maxSum) / Double(k)
+    }
+    
+    // MARK: - 674. 最长连续递增序列
+    static func findLengthOfLCIS(_ nums: [Int]) -> Int {
+        var curLength = 1
+        var maxLength = 1
+        for i in 1..<nums.count {
+            if nums[i] - nums[i-1] > 0 {
+                curLength += 1
+                maxLength = max(maxLength, curLength)
+            } else {
+                curLength = 1
+            }
+        }
+        return maxLength
+    }
+    
+    // MARK: - 209. 长度最小的子数组（中等）
+    static func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
+        var minLength = Int.max
+        var curSum = 0
+        var left = 0
+        var right = 0
+        while right < nums.count {
+            curSum += nums[right]
+            while curSum >= target {
+                let curMin = right - left + 1
+                if curMin == 1 {
+                    return curMin
+                }
+                minLength = min(minLength, curMin)
+                curSum -= nums[left]
+                left += 1
+            }
+            right += 1
+        }
+        return minLength == Int.max ? 0 : minLength
+    }
+    
+    // MARK: - 3. 无重复字符的最长子串（中等）
+    static func lengthOfLongestSubstring(_ s: String) -> Int {
+        let chars = Array(s)
+        var ans = 0
+        var right = 0
+        var curChars = [Character]()
+        while right < chars.count {
+            let char = chars[right]
+            while curChars.contains(char) {
+                curChars.removeFirst()
+            }
+            curChars.append(char)
+            ans = max(ans, curChars.count)
+            right += 1
+        }
+        return ans
+    }
+    
+    // MARK: - 438. 找到字符串中所有字母异位词（中等）
+    static func findAnagrams(_ s: String, _ p: String) -> [Int] {
+        func getNum(_ char: Character) -> Int {
+            return Int(char.asciiValue! - ("a" as Character).asciiValue!)
+        }
+        /*
+        let sLength = s.count
+        let pLength = p.count
+        guard sLength >= pLength else { return [] }
+        let sChars = Array(s)
+        let pChars = Array(p)
+        var ans = [Int]()
+        var sCount = Array(repeating: 0, count: 26)
+        var pCount = Array(repeating: 0, count: 26)
+        for i in 0..<p.count {
+            sCount[getNum(sChars[i])] += 1
+            pCount[getNum(pChars[i])] += 1
+        }
+        if sCount == pCount {
+            ans.append(0)
+        }
+        for i in 0..<sLength-pLength {
+            sCount[getNum(sChars[i])] -= 1
+            sCount[getNum(sChars[i + pLength])] += 1
+            if sCount == pCount {
+                ans.append(i + 1)
+            }
+        }
+        return ans
+         */
+        
+        // 优化
+        let sLength = s.count
+        let pLength = p.count
+        guard sLength >= pLength else { return [] }
+        let sChars = Array(s)
+        let pChars = Array(p)
+        var ans = [Int]()
+        var countArr = Array(repeating: 0, count: 26)
+        for i in 0..<pLength {
+            countArr[getNum(sChars[i])] += 1
+            countArr[getNum(pChars[i])] -= 1
+        }
+        // diff代表p和s中不同字母数量
+        var diff = 0
+        for i in 0..<26 {
+            if countArr[i] != 0 {
+                diff += 1
+            }
+        }
+        if diff == 0 {
+            ans.append(0)
+        }
+        
+        for i in 0..<sLength-pLength {
+            // s中将要移除的元素数量为1，diff减1;s中将要移除的元素数量为0，diff加1
+            if countArr[getNum(sChars[i])] == 1 {
+                diff -= 1
+            } else if countArr[getNum(sChars[i])] == 0 {
+                diff += 1
+            }
+            countArr[getNum(sChars[i])] -= 1
+            
+            // s中将要添加的元素数量为-1，diff减1;s中将要添加的元素数量为0，diff加1
+            if countArr[getNum(sChars[i + pLength])] == -1 {
+                diff -= 1
+            } else if countArr[getNum(sChars[i + pLength])] == 0 {
+                diff += 1
+            }
+            countArr[getNum(sChars[i + pLength])] += 1
+            if diff == 0 {
+                ans.append(i + 1)
+            }
+        }
+        return ans
+    }
+    
+    // MARK: - 567. 字符串的排列（中等）
+    static func checkInclusion(_ s1: String, _ s2: String) -> Bool {
+        func getNum(_ char: Character) -> Int {
+            return Int(char.asciiValue! - ("a" as Character).asciiValue!)
+        }
+        let s1Len = s1.count
+        let s2Len = s2.count
+        guard s2Len >= s1Len else { return false }
+        let s1Chars = Array(s1)
+        let s2Chars = Array(s2)
+        var countArr = Array(repeating: 0, count: 26)
+        for i in 0..<s1Len {
+            countArr[getNum(s2Chars[i])] += 1
+            countArr[getNum(s1Chars[i])] -= 1
+        }
+        var diff = 0
+        for i in 0..<26 {
+            if countArr[i] != 0 {
+                diff += 1
+            }
+        }
+        if diff == 0 {
+            return true
+        }
+        for i in 0..<s2Len-s1Len {
+            if countArr[getNum(s2Chars[i])] == 1 {
+                diff -= 1
+            } else if countArr[getNum(s2Chars[i])] == 0 {
+                diff += 1
+            }
+            countArr[getNum(s2Chars[i])] -= 1
+            
+            if countArr[getNum(s2Chars[i + s1Len])] == -1 {
+                diff -= 1
+            } else if countArr[getNum(s2Chars[i + s1Len])] == 0 {
+                diff += 1
+            }
+            countArr[getNum(s2Chars[i + s1Len])] += 1
+            if diff == 0 {
+                return true
+            }
+        }
+        return false
+    }
+    
+    // MARK: - 424. 替换后的最长重复字符（中等）
+    static func characterReplacement(_ s: String, _ k: Int) -> Int {
+        func getNum(_ char: Character) -> Int {
+            return Int(char.asciiValue! - Character("A").asciiValue!)
+        }
+        let length = s.count
+        var countArr = [Int](repeating: 0, count: 26)
+        let sChars = Array(s)
+        var left = 0
+        var right = 0
+        var maxLength = 0
+        while right < length {
+            countArr[getNum(sChars[right])] += 1
+            // 右指针和之前的所有字符的最多重复字符个数
+            maxLength = max(maxLength, countArr[getNum(sChars[right])])
+            // 最多的重复字符的个数 + k 小于 当前总字符数，则无法替换k个字符后所有字符相等，将左指针右指针同时右移，保证right-left不变
+            if right - left + 1 - maxLength > k {
+                countArr[getNum(sChars[left])] -= 1
+                left += 1
+            }
+            right += 1
+        }
+        return right - left
+    }
+    
+    // MARK: - 76. 最小覆盖子串（困难）
+    static func minWindow(_ s: String, _ t: String) -> String {
+        func check(_ map1: [Character : Int], _ map2: [Character : Int]) -> Bool {
+            for (c, count) in map1 {
+                if map2[c] == nil {
+                    return false
+                }
+                if let map2Count = map2[c], map2Count < count {
+                    return false
+                }
+            }
+            return true
+        }
+        
+        let sChars = Array(s)
+        let tChars = Array(t)
+        let sLen = sChars.count
+        let tLen = tChars.count
+        guard sLen >= tLen else { return "" }
+        var tMap = [Character : Int]()
+        var sMap = [Character : Int]()
+        for c in tChars {
+            tMap[c] = (tMap[c] ?? 0) + 1
+        }
+        var left = 0
+        var right = 0
+        var minLen = Int.max
+        var ansLeft = -1
+        var ansRight = -1
+        while right < sLen {
+            if tMap.keys.contains(sChars[right]) {
+                sMap[sChars[right]] = (sMap[sChars[right]] ?? 0) + 1
+            }
+            while check(tMap, sMap) {
+                if right - left + 1 < minLen {
+                    minLen = right - left + 1
+                    ansLeft = left
+                    ansRight = right
+                }
+                if tMap.keys.contains(sChars[left]) {
+                    sMap[sChars[left]] = (sMap[sChars[left]] ?? 0) - 1
+                }
+                left += 1
+                
+            }
+            right += 1
+        }
+        return ansLeft == -1 ? "" : String(sChars[ansLeft...ansRight])
+    }
+    
+    // MARK: - 30. 串联所有单词的子串
+    static func findSubstring(_ s: String, _ words: [String]) -> [Int] {
+        let sChars = Array(s)
+        let sLen = sChars.count
+        var ans = [Int]()
+        // 单词数量
+        let wordsCount = words.count
+        // 单词长度
+        let wordLen = words[0].count
+        for i in 0..<wordLen {
+            if i + wordsCount * wordLen > sLen {
+                break
+            }
+            var diff = [String : Int]()
+            for j in 0..<wordsCount {
+                let word = String(sChars[i+j*wordLen..<i+(j + 1)*wordLen])
+                diff[word] = (diff[word] ?? 0) + 1
+            }
+            for word in words {
+                diff[word] = (diff[word] ?? 0) - 1
+                if diff[word] == 0 {
+                    diff[word] = nil
+                }
+            }
+            
+            var start = i
+            while start < sLen - wordsCount * wordLen + 1 {
+                if start != i {
+                    var word = String(sChars[start + (wordsCount - 1) * wordLen..<start + wordsCount * wordLen])
+                    diff[word] = (diff[word] ?? 0) + 1
+                    if diff[word] == 0 {
+                        diff[word] = nil
+                    }
+                    word = String(sChars[start - wordLen..<start])
+                    diff[word] = (diff[word] ?? 0) - 1
+                    if diff[word] == 0 {
+                        diff[word] = nil
+                    }
+                }
+                if diff.isEmpty {
+                    ans.append(start)
+                }
+                start += wordLen
+            }
+            
+        }
+        return ans
+    }
+    
+    // MARK: - 86. 分隔链表（中等）
+    static func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
+        let dummyLeft = ListNode()
+        dummyLeft.next = head
+        var left: ListNode? = dummyLeft
+        let dummyRight = ListNode()
+        dummyRight.next = head
+        var right: ListNode? = dummyRight
+        var cur: ListNode? = head
+        while cur != nil {
+            if cur!.val < x {
+                left?.next = cur
+                left = left?.next
+            } else {
+                right?.next = cur
+                right = right?.next
+            }
+            cur = cur?.next
+        }
+        right?.next = nil
+        left?.next = dummyRight.next
+        return dummyLeft.next
+    }
+    
+    // MARK: - 328. 奇偶链表（中等）
+    static func oddEvenList(_ head: ListNode?) -> ListNode? {
+        let oddDummy = ListNode()
+        let evenDummy = ListNode()
+        var oddHead: ListNode? = oddDummy
+        var evenHead: ListNode? = evenDummy
+        var cur: ListNode? = head
+        var isEven = true
+        while cur != nil {
+            isEven = !isEven
+            if isEven {
+                evenHead?.next = cur
+                evenHead = evenHead?.next
+            } else {
+                oddHead?.next = cur
+                oddHead = oddHead?.next
+            }
+            cur = cur?.next
+        }
+        evenHead?.next = nil
+        oddHead?.next = evenDummy.next
+        return oddDummy.next
+    }
+    
+    // MARK: - 160. 相交链表
+    static func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+        var curA = headA
+        var curB = headB
+        while curA !== curB {
+            curA = curA == nil ? headB : curA?.next
+            curB = curB == nil ? headA : curB?.next
+        }
+        return curA
+    }
 }
